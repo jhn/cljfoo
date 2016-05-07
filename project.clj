4e63b6e4a691a28d6b06e3fe3d866584c3f03ccc
Johan Mena <johan.menac@gmail.com>
2016-05-07 13:09:07 -0400

    Add JSON support

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [ring/ring-jetty-adapter "1.4.0"]]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-json "0.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :global-vars {*warn-on-reflection* true}
   :ring {:handler cljfoo.handler/app
--- src/cljfoo/handler.clj
+++ src/cljfoo/handler.clj
@@ -2,7 +2,8 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.adapter.jetty :refer [run-jetty]]
-            [ring.middleware.defaults :refer [wrap-defaults api-defaults]])
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]])
   (:gen-class))
 
 (defroutes app-routes
@@ -10,7 +11,10 @@
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes api-defaults))
+  (-> app-routes
+      (wrap-defaults api-defaults)
+      (wrap-json-body {:keywords? true})
+      (wrap-json-response)))
 
 (defn -main [& args]
   (run-jetty app {:port 3000}))
